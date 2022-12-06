3ed553250190deb93a47c9187458dd5af6294137
Zuma <3umchik24@mail.ru>
2022-12-06 18:05:41 +0300

    Задача №13 Д/з

--- Ex10/Program.cs
+++ Ex10/Program.cs
@@ -5,7 +5,7 @@ while (num>999 | num<100)
 {
     Console.WriteLine($"Число не входит в диапазон");
     Console.Write("Введите трёхзначное число: ");
-    int num1 = Convert.ToInt32(Console.ReadLine());
-    num = num1;
+    num = Convert.ToInt32(Console.ReadLine());
+
 }
 Console.WriteLine($"2-ая цифра числа {num} - {num%100/10}");
---
+++ Ex13/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите любое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num/100 == 0)
+    Console.Write($"3-ей цифры нет в числе {num}");
+else 
+     Console.Write($"Третья цифра числа {num} - {num%1000/100}");
