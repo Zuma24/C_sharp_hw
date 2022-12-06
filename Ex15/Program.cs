a4f9ef8dd0b1cf2d8881d89030d290b1e05a2ea5
Zuma <3umchik24@mail.ru>
2022-12-06 21:21:57 +0300

    Задача №15 Д/з

---
+++ Ex15/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите номер дня недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num>0 & num<8)
+{
+    if (num<6)
+    {
+        Console.Write("Рабочий день");
+    }
+    else
+    {
+        Console.Write("Выходной");
+    }
+}
+else
+    Console.Write("Нет таких дней недели");
