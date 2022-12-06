e2d3e7095b626c5d472c8aaec7b4b6ad7a2fa334
Zuma <3umchik24@mail.ru>
2022-12-06 17:30:04 +0300

    Задача №10 Д/з

--- Ex10/Program.cs
+++ Ex10/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+while (num>999 | num<100)
+{
+    Console.WriteLine($"Число не входит в диапазон");
+    Console.Write("Введите трёхзначное число: ");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    num = num1;
+}
+Console.WriteLine($"2-ая цифра числа {num} - {num%100/10}");
