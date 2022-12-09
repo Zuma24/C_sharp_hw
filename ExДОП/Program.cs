efba92563a3c4e0be3fa89dacb21a6c52fdc3a92
Zuma <3umchik24@mail.ru>
2022-12-10 00:16:09 +0300

    Дополнительная задача - Второй максимум

---
+++ ExДОП/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.Write("Введите числа: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int max1 = num1;
+int max2 = num1;
+while (num1<100 & num1!=0)
+{
+    Console.Write("Введите число: ");
+    num1 = Convert.ToInt32(Console.ReadLine());
+    if (num1 > max1)
+    {
+        max2 = max1;
+        max1 = num1;
+    }
+    else if (num1>max2)
+    {
+        max2=num1;
+    }
+}
+Console.WriteLine($"Первый максимум: {max1} \nВторой максимум: {max2} ");
