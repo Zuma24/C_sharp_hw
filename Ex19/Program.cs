f2375bce399927a40cfed8551845f2062e699013
Zuma <3umchik24@mail.ru>
2022-12-10 11:46:24 +0300

    Задача № 19

---
+++ Ex19/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num<99999 && num>10000)
+{
+    if (num/10000==num%10 && num/1000%10==num%100/10)
+    {
+        Console.Write($"{num} - палиндром");
+    }
+    else 
+    {
+        Console.Write($"Число {num} не палиндром");
+    }
+}
+else 
+    Console.Write($"{num} не пятизначное число");
