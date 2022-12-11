7e4a34fd49aa8c5b34f3cf75b08eb243baee439e
Zuma <3umchik24@mail.ru>
2022-12-11 09:35:45 +0300

    Задача №23

--- Ex23/Program.cs
+++ Ex23/Program.cs
@@ -3,5 +3,5 @@ Console.Write("Введите любое число: ");
 int N = Convert.ToInt32(Console.ReadLine());
 for (int i=1; i<=N; i++)
 {
-    Console.Write($"{i*i*i} ");
+    Console.Write($"{Math.Pow(i,3)} ");
 }
