6d80f4bfd8979181332aa4add7eb39a6190619d3
Zuma <3umchik24@mail.ru>
2022-12-17 02:36:31 +0300

    Задание №25

---
+++ Ex25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+double num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+double num2 = Convert.ToInt32(Console.ReadLine()), i = 1, num = num1;
+while (i<num2)
+{
+	num *=num1;
+    i++;
+}
+Console.WriteLine(num);
