6b31211cebb78c26f2c9c8b4106cb7e164ce1bd4
Zuma <3umchik24@mail.ru>
2022-12-17 03:22:07 +0300

    Задание №27

---
+++ Ex27/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+string? n = Console.ReadLine();
+int num2=0, num1;
+for (int i = 0; i < n.Length; i++)
+{
+    num1 = int.Parse(n[i].ToString());
+	num2 += num1; 
+}
+Console.WriteLine(num2);
