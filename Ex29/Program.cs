7e68e1fe796d4a780ce016ea6f0dab6be989fcb2
Zuma <3umchik24@mail.ru>
2022-12-17 06:33:28 +0300

    Задание №29

---
+++ Ex29/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+	array[i] = Convert.ToInt32(Console.ReadLine()); 
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
+
+//string[] num = Console.ReadLine().Split(",");
+
