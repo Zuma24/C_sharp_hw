c28a1d48325b78a438b34e16d74b13ffd5c907a1
Zuma <3umchik24@mail.ru>
2022-12-17 06:55:00 +0300

    Задание №29

--- Ex29/Program.cs
+++ Ex29/Program.cs
@@ -1,11 +1,19 @@
-﻿Console.Clear();
+﻿//Вводит пользователь
+Console.Clear();
 Console.Write("Введите размер массива: ");
 int n = Convert.ToInt32(Console.ReadLine());
 int[] array = new int[n];
 for (int i = 0; i < array.Length; i++)
 	array[i] = Convert.ToInt32(Console.ReadLine()); 
-
 Console.WriteLine($"[{string.Join(", ", array)}]");
 
-//string[] num = Console.ReadLine().Split(",");
+//Ввод случайных чисел
 
+
+Console.Write("Введите размер массива: ");
+int nu = Convert.ToInt32(Console.ReadLine());
+int[] num = new int[nu];
+for (int i = 0; i < num.Length; i++)
+	num[i] = new Random().Next(0,1000); 
+
+Console.WriteLine($"[{string.Join(", ", num)}]");
